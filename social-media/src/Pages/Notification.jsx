c57ade6e61db2ebe405f013d0974e1be71cba2c1
mwaniki-Titus <mwanikititus1@gmail.com>
2024-02-05 08:26:11 -0800

    added notification styling

--- social-media/src/Pages/Notification.jsx
+++ social-media/src/Pages/Notification.jsx
@@ -1,6 +1,5 @@
-// Notification.jsx
-
 import React from 'react';
+import PropTypes from 'prop-types';
 import xicon from '../assets/images/close.close.png';
 import wade from '../assets/images/Avatar.png';
 import jane from '../assets/images/Avatar1.png';
@@ -10,36 +9,32 @@ import brooklyn from '../assets/images/Avatar4.png';
 import './Notification.scss';
 
 const friends = [
-  {
-    icon: wade,
-    name: 'Angela Lee',
-  },
-  {
-    icon: jane,
-    name: 'Jane Cooper',
-  },
-  {
-    icon: esther,
-    name: 'Esther Howard',
-  },
-  {
-    icon: cameron,
-    name: 'Cameron Williamson',
-  },
-  {
-    icon: brooklyn,
-    name: 'Brooklyn Simmons',
-  },
+  { icon: wade, name: 'Angela Lee' },
+  { icon: jane, name: 'Jane Cooper' },
+  { icon: esther, name: 'Esther Howard' },
+  { icon: cameron, name: 'Cameron Williamson' },
+  { icon: brooklyn, name: 'Brooklyn Simmons' },
 ];
 
-const Notifications = ({ onClose }) => {
+const Notification = ({ onClose }) => {
+  const renderFriends = (friendList) => (
+    <div className="people">
+      {friendList.map(({ icon, name }, index) => (
+        <div key={index} className="friend">
+          <img src={icon} alt={`${name} Avatar`} />
+          <p>{name}</p>
+        </div>
+      ))}
+    </div>
+  );
+
   return (
     <div className="bigmain">
       <div className="notification">
         <div className="noti">
           <h4>Notification</h4>
           <button onClick={onClose}>
-            <img src={xicon} alt="X Icon" />
+            <img src={xicon} alt="Close Icon" />
           </button>
         </div>
         <div className="allnote">
@@ -49,22 +44,21 @@ const Notifications = ({ onClose }) => {
         <div className="tod">
           <h4>Today</h4>
         </div>
-        <div className="people">
-          {friends.map((friend, index) => (
-            <div key={index} className="friend">
-              <img src={friend.icon} alt={friend.name} />
-              <p>{friend.name}</p>
-            </div>
-          ))}
-        </div>
+        {renderFriends(friends)}
         <div className="yesterday">
           <h4>YESTERDAY</h4>
+          {renderFriends(friends)}
         </div>
       </div>
     </div>
   );
 };
 
-export default Notifications;
+Notification.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Notification;
+
 
 
